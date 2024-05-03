694d4dd4addd20207f9fa675fadc0a45aba307e7
Lubomyr Chorniak <lubko2802@gmail.com>
2024-05-03 21:09:36 +0300

    Removed storyboard

---
+++ TelegrameClone/TelegrameClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TelegrameClone
+//
+//  Created by Lubomyr Chorniak on 03.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        
+    }
+
+
+}
+

914670e4028b4104e2f666e004caa8faadb45464
Lubomyr Chorniak <lubko2802@gmail.com>
2024-05-04 15:03:03 +0300

    Added coordinator protocol

---
+++ TelegrameClone/TelegrameClone/NavigationLayer/Coordinators/Coordinator.swift
@@ -0,0 +1,38 @@
+//
+//  Coordinator.swift
+//  TelegrameClone
+//
+//  Created by Lubomyr Chorniak on 03.05.2024.
+//
+
+import UIKit
+
+class Coordinator: CoordinatorProtocol {
+    var childCoordinators: [CoordinatorProtocol]
+    var type: CoordinatorType
+    var navigationController: UINavigationController?
+    weak var finishDelegate: CoordinatorFinishDelegate?
+    
+    init(childCoordinators: [CoordinatorProtocol] = [CoordinatorProtocol](), type: CoordinatorType, navigationController: UINavigationController, finishDelegate: CoordinatorFinishDelegate? = nil) {
+        self.childCoordinators = childCoordinators
+        self.type = type
+        self.navigationController = navigationController
+        self.finishDelegate = finishDelegate
+    }
+    
+    deinit {
+        print("Coordinator deinited \(type)")
+        childCoordinators.forEach { $0.finishDelegate = nil }
+        childCoordinators.removeAll()
+    }
+    
+    func start() {
+        print("Coordinator start")
+    }
+    
+    func finish() {
+        print("Coordinator finish")
+    }
+    
+    
+}
---
+++ TelegrameClone/TelegrameClone/NavigationLayer/Coordinators/CoordinatorProtocol.swift
@@ -0,0 +1,43 @@
+//
+//  CoordinatorProtocol.swift
+//  TelegrameClone
+//
+//  Created by Lubomyr Chorniak on 03.05.2024.
+//
+
+import UIKit
+
+enum CoordinatorType {
+    case app
+    case onboarding
+    case registration
+    case contacts
+    case chats
+    case settings
+}
+
+protocol CoordinatorProtocol: AnyObject {
+    var childCoordinators: [CoordinatorProtocol] { get set }
+    var type: CoordinatorType { get }
+    var navigationController: UINavigationController? { get set }
+    var finishDelegate: CoordinatorFinishDelegate? { get set }
+    
+    func start()
+    func finish()
+}
+
+extension CoordinatorProtocol {
+    func addChildCoordinator(_ childCoordinator: CoordinatorProtocol) {
+        childCoordinators.append(childCoordinator)
+    }
+    func removeChildCoordinator(_ childCoordinator: CoordinatorProtocol) {
+        childCoordinators = childCoordinators.filter{ $0 !== childCoordinator }
+    }
+}
+
+protocol CoordinatorFinishDelegate: AnyObject {
+    func coordinatorDidFinish(childCoordinatro: CoordinatorProtocol)
+}
+
+
+

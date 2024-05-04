a6a2ec62a2dccfb590c436875943c2bd5532f9b2
Lubomyr Chorniak <lubko2802@gmail.com>
2024-05-04 15:11:49 +0300

    Added UserStorage

---
+++ TelegrameClone/TelegrameClone/ServicesLayer/UserStorage.swift
@@ -0,0 +1,19 @@
+//
+//  UserStorage.swift
+//  TelegrameClone
+//
+//  Created by Lubomyr Chorniak on 04.05.2024.
+//
+
+import Foundation
+
+class UserStorage {
+    
+    static let shared = UserStorage()
+    
+    var passedOnboarding: Bool {
+        get { UserDefaults.standard.bool(forKey: "passedOnboarding") }
+        set { UserDefaults.standard.set(newValue, forKey: "passedOnboarding") }
+    }
+    
+}
